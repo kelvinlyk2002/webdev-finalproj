84413b9f10608d6809d14cba6ddb331966b30d32
kelvinlyk2002 <kelvinlyk2002@gmail.com>
2020-08-27 23:00:43 +0800

    progress

--- data/originData.js
+++ data/originData.js
@@ -5,21 +5,63 @@ var eventList = {
                         imagePath: "img/01.jpg",
                         textPosAdj: "order-lg-1",
                         title: "Handover",
-                        comment: "123"
+                        comment: "Back in 1997 when HK was handed over to China, the people of HK actually did look forward to it. Despite most being politically apathetic, HK people hoped for a prosperous future while the Western world hoped HK can bring change to the still-socialist China. None happened."
                     },
                     {
                         imagePosAdj: "",
                         imagePath: "img/01.jpg",
                         textPosAdj: "",
-                        title: "Handover",
-                        comment: "123"
+                        title: "SARS",
+                        comment: "1997, asia financial crisis, 2000 dot-com bubble and 2003 SARS outbreak has stalled the HK economy. Chinese government responded by flooding tourist to HK and liberalization of HK people doing business in China. Along with the economic takeoff in China, this business community builds up a significant pro-China community in HK."
                     },
                     {
                         imagePosAdj: "order-lg-2",
                         imagePath: "img/01.jpg",
                         textPosAdj: "order-lg-1",
-                        title: "Handover",
-                        comment: "123"
+                        title: "First denial of political reform in 2005",
+                        comment: "The first term of government after the handover was appointed by China. The Chief Executive (head of HK government) was elected by a small group appointed by China while only 50% of the Legislative Council (parliament of HK) is democractically elected. As part of the handover deal, China promised there would be universial sufferage to the Chief Executive and the Legislative Council. In 2005, the HK government proposed only a minor change to this method which excluded the universial sufferage. This proposal was eventually denied."
+                    },
+                    {
+                        imagePosAdj: "",
+                        imagePath: "img/01.jpg",
+                        textPosAdj: "",
+                        title: "2008 Beijing Olympic",
+                        comment: "In 2008 when Beijing had its Olympic, China nationalism is at all time high and more people associated themselves as Chinese than ever. Sichuan, China had an earthquake of magnitude-8, HKers donated over HK$1 Billion (US$128 Million) within a week."
+                    },
+                    {
+                        imagePosAdj: "order-lg-2",
+                        imagePath: "img/01.jpg",
+                        textPosAdj: "order-lg-1",
+                        title: "Aftermath of the 2009 financial tsunami",
+                        comment: "In 2009, HK economy again went into recession. China, while still not closely tied to world economy, suffered relatively minorly. In 1997, Hong Kong's GDP is 18% of China's. In 2009, it has shrinked to 4%. In a bad financial shape, the HK government decided to spend HK$66.9 billion on the construction of HK Express Rail Link, a high speed rail connecting to Guangzhou. Further to the absence of universal sufferage promised in the handover, the popularity of the government starts to deteriorate. The controversy gave birth to the first generation of political aware citizens."
+                    },
+                    {
+                        imagePosAdj: "",
+                        imagePath: "img/01.jpg",
+                        textPosAdj: "",
+                        title: "Political reform in 2010",
+                        comment: "In 2010, the government proposed another political reform, mostly the same as the 2005 denied reform and still no universal sufferage. This proposal got through with the predominant pro-Beijing legislators."
+                    },
+                    {
+                        imagePosAdj: "order-lg-2",
+                        imagePath: "img/01.jpg",
+                        textPosAdj: "order-lg-1",
+                        title: "Rising conflict in the borders",
+                        comment: "Post 2008 financial tsunami, the United States started Quantitative Easing. HK$ is pegged to US$ and started to depreciate as well. Chinese started to pour into HK in search of higher quality groceries, better medical services while enjoying a great discount of the HK$. While the business sector welcomed the new monies, HKers living in the border communities started to find their neighbourhood being disrupted by the Chinese. HK mothers have difficulties finding hospital to give birth. In 2012, a march called 'Reclaim Sheung Shui Station' was held to push back smugglers in Sheung Shui, a station away from Shenzhen. This is the first sign of turning tide against China in Hong Kong. According to the poll done by the University of Hong Kong, people's net satisfactoon of the government dropped from +36% to -36% over the course of 2008-2011."
+                    },
+                    {
+                        imagePosAdj: "",
+                        imagePath: "img/01.jpg",
+                        textPosAdj: "",
+                        title: "2012-13 Anti National Education Movements",
+                        comment: "In 2012, the HK government tried to establish a 'National Education' into the primary school's curriculum in order to please China. Along with denial of free-to-air television license to HKTV, which the founder is from a non-Beijing background, over 120,000 made frequent marches and assembly near Civic Plaza, Admiralty. Joshua Wong, now renowned activist, first made public appearance. He and his organization, Scholarism, successfully stopped the 'National Education'."
+                    },
+                    {
+                        imagePosAdj: "order-lg-2",
+                        imagePath: "img/01.jpg",
+                        textPosAdj: "order-lg-1",
+                        title: "Umbrella Movement",
+                        comment: "In 2013, a low point "
                     }
                 ]
-};
+            };
