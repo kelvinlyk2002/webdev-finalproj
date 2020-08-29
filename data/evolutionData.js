e7c421ddf99f0efb88a30566ece1dc5921a48692
kelvinlyk2002 <kelvinlyk2002@gmail.com>
2020-08-29 22:17:29 +0800

    progress

--- data/evolutionData.js
+++ data/evolutionData.js
@@ -4,21 +4,21 @@ var eventList = {
                         imagePosAdj: "order-lg-2",
                         imagePath: "img/01.jpg",
                         textPosAdj: "order-lg-1",
-                        title: "Handover",
-                        comment: "123"
+                        title: "Murder of Poon Hiu-wing & Introduction of the extradiction bill",
+                        comment: "Poon was murdered by Chan tong-kai in Taipei in early 2018. HK government refused Taiwan's request for single extradiction saying that both Poon and Chan are HK citizens, but proposed an extradiction bill that would allow extradiction not only to Taiwan, but China. Rumours said it was the result of the lobbying of Chinese authorities, targeting HK activist & political refugees from China."
                     },
                     {
                         imagePosAdj: "",
                         imagePath: "img/01.jpg",
                         textPosAdj: "",
-                        title: "Handover",
-                        comment: "123"
+                        title: "Earlier rallies & 9 June protest",
+                        comment: "Active political figures organized some small rallies between March to May. There was also a lawyers' silent march in 6 June. Unmoved, the government retained its voting of the bill on 12 June. CHRF, a joint organization for the pan-democractic fractions in HK, organized a protest which over 1.03 million participated. While "
                     },
                     {
                         imagePosAdj: "order-lg-2",
                         imagePath: "img/01.jpg",
                         textPosAdj: "order-lg-1",
-                        title: "Handover",
+                        title: "June",
                         comment: "123"
                     }
                 ]
