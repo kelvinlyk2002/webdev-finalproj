b93288f526ceda9aa9dd803e4b3648a2b6138cfe
kelvinlyk2002 <kelvinlyk2002@gmail.com>
2020-09-05 15:37:00 +0800

    progress

--- data/universitiesData.js
+++ data/universitiesData.js
@@ -28,7 +28,7 @@ var eventList = {
                     {
                         imagePath: "assets/img/1111cuhk.png",
                         title: "11 November – CUHK",
-                        comment: "CUHK's Number 2 bridge cross over Tolo Highway and the railway of East Rail Line. In response to the call for general strike, protestors dropped objects to the highway and the railway. Starting from 6AM, police has been in a standoff with the protestors on Number 2 bridge, and shooting tear gas into the university campus."
+                        comment: "CUHK's No. 2 bridge cross over Tolo Highway and the railway of East Rail Line. In response to the call for general strike, protestors dropped objects to the highway and the railway. Starting from 6AM, police has been in a standoff with the protestors on No. 2 bridge, and shooting tear gas into the university campus."
                     },
                     {
                         imagePath: "assets/img/1112central.jpg",
@@ -36,14 +36,49 @@ var eventList = {
                         comment: "For the first time after months of protests, tear gas was deployed in the Central district when office workers organized a flash-mob protest at lunchtime."
                     },
                     {
-                        imagePath: "assets/img/1111pregnantwoman.gif",
+                        imagePath: "assets/img/1112kowloontong.jpg",
                         title: "12 November – Kowloon Tong",
-                        comment: "The police also confronted with the protesters outside City University of Hong Kong, who threw objects from the footbridge in an attempt to block the traffic in Kowloon Tong. Nearby, protesters marched into the Festival Walk mall in Kowloon Tong after the mall closed early and set a giant Christmas tree on fire, in response to the mall's previous assistance to police "
+                        comment: "The police also confronted with the protesters outside City University of Hong Kong, who threw objects from the footbridge in an attempt to block the traffic in Kowloon Tong. Nearby, protesters marched into the Festival Walk mall in Kowloon Tong after the mall closed early and set a giant Christmas tree on fire, in response to the mall's collusion with the police, resulting in injuries and arrests on 10 November."
                     },
                     {
-                        imagePath: "assets/img/1111saiwanho.gif",
-                        title: "11 November – Dawn Action - Sai Wan Ho",
-                        comment: "In Sai Wan Ho, a police shot unarmed protestors with live bullets. A masked young man wearing a white hoodie is said to have shouted at a traffic policeman. The man raised his hands once to show that he held no weapons. The traffic policeman pointed a gun at the man's chest and quickly locked him in a choke hold while still holding the gun, pointing it at pedestrians. One unarmed 21-year-old man in black was shot once in the abdomen and fell on the zebra crossing between MTR Exit B and Tai On Building. A third young man, dressed in black, approached and was also shot, once or twice. In total, the traffic policeman fired three live rounds."
+                        imagePath: "assets/img/1112cuhkfrontline.jpg",
+                        title: "12/13 November – CUHK - No. 2 Bridge",
+                        comment: "On the second day of the conflict, protesters continued to attempt to block traffic. As early as 4AM, police entered CUHK area and cleared some of the roadblocks. At 7 AM, police began a standoff with the protesters, who attempted to stop the police from clearing the roadblocks set on the bridge. Negotiations broke off as the police advanced and began firing tear gas into the campus, while the student protesters, in response, hurled bricks and petrol bombs.The police entered the university through the No. 2 Bridge, arrested several people inside the campus, and shot canisters of tear gas and rubber bullets at the Sir Philip Haddon-Cave Sports Field. The standoff lasted until 3PM, as police holding the bridge. Contrary to their claim to reopen Tolo Highway, they did not clear the roadblocks beneath the No. 2 Bridge, which is now held by the police. On 4PM, they used tear gas and rubber bullets to push back protestors and surrounded the university campus. Vice-Chancellor of CUHK attempted to broker peace between the police and the protestors, only to find the police advancing while claiming to be retreating. Tear gas are continued to be aimlessly fired, onto open areas such as the Sports Field. The only purpose was to make the air unbreathable for anyone in the campus. From 7PM, non-stop tear gas was being shot to the campus for 2 hours. On the frontline of the scene, No.2 Bridge, many were wounded by the flying cannisters of tear gas and rubber bullets. The clashes were among the most relentless in Hong Kong’s pro-democracy movement, as police could be heard firing their weapons continuously for hours. It was reported that they had used about 1,567 canisters of tear gas, 1,312 shots of rubber bullets, 380 shots of bean bag rounds, and 126 shots of sponge grenades in CUHK. That was more than 20 times more used than in 2014, in a single day. The liquids used in the chemical cannons contained chemicals that burned when in touch with eyes/skin. In the fierce fight lasted for hours on the No. 2 Bridge, a volunteer paramedic reported that at least 70 protesters were injured and all external medical services were barred by the police. By 10PM, police retreated from the fight of No. 2 Bridge, but remained stationed not far away on Tolo Highway. By midnight, riot police gathered again beneath No.2 Bridge, but subsequently retreated after shooting rounds of tear gas. Protestors took turn to watch the bridge over night."
+                    },
+                    {
+                        imagePath: "assets/img/1112cuhksupport.jpg",
+                        title: "12/13 November – CUHK - Supports out of campus",
+                        comment: "CUHK has the largest campus in Hong Kong, with 137.3 ha in area. While the police could not easily surround the campus, the protestors also needed huge logistics support to defend it. During the clash, many netizens called for simultaneous protest ('blossom') in Sha Tin & Ma On Shan, attempting to pin down the police forces available around CUHK. Over 18 communities in Hong Kong have clashes with the police. Since Tolo Highway is blocked, the only transport between New Territories East & urban area is Tai Po Road, where protestors occupied to ensure police cannot attack the campus from the back. The traffic built up for kilometers, many leaving their cars as a barricade. Motorcyclists carried protestors and supplies, traversing in the lines."
+                    },
+                    {
+                        imagePath: "assets/img/1115cuhk.jpg",
+                        title: "15 November – CUHK",
+                        comment: "Minor clashes between students and police occurs occasionally, while protestors maintaining their stronghold in CUHK. However, as some argued that the occupation of the university did not align with the fluidity the protesters have exhibited since the protests started, protesters began to leave CUHK on Friday night as they moved to PolyU."
+                    },
+                    {
+                        imagePath: "assets/img/1112cuhksupport.jpg",
+                        title: "12/13 November – CUHK - Support from outside of campus",
+                        comment: "CUHK has the largest campus in Hong Kong, with 137.3 ha in area. While the police could not easily surround the campus, the protestors also needed huge logistics support to defend it. During the clash, many netizens called for simultaneous protest ('blossom') in Sha Tin & Ma On Shan, attempting to pin down the police forces available around CUHK. Over 18 communities in Hong Kong have clashes with the police. Since Tolo Highway is blocked, the only transport between New Territories East & urban area is Tai Po Road, where protestors occupied to ensure police cannot attack the campus from the back. The traffic built up for kilometers, many leaving their cars as a barricade. Motorcyclists carried protestors and supplies, traversing in the lines."
+                    },
+                    {
+                        imagePath: "assets/img/1117poly.jpg",
+                        title: "17 November – PolyU",
+                        comment: "Protesters initially set-up a roadblock at the entrance of Cross-Harbour Tunnel, close to PolyU, and took the university as the base to defend it from attacks by. As situation in CUHK has calmed, some moved here to continue the movement. At around 10:00 am, the government sent pro-Beijing citizens to attempt clearing the roadblocks between Austin Road and Chatham Road South, which was denied by the protestors. Riot police arrived at around 11:00 and confronted the protesters, firing tear gas and rubber bullets at them till 5pm, starting to besige the university. They deployed 2 chemical cannons vehicles and 2 armoured vehicles, trying to forcibly advance through the frontline on the bridge overseeing cross harbour tunnel, one of the heaviest traffic tunnel."
+                    },
+                    {
+                        imagePath: "assets/img/1117polymedic.jpg",
+                        title: "17 November – PolyU - arrest of medics",
+                        comment: "Around 9:00 pm, police publicly announced that anyone arrested inside Polytechnic University would face the charge of rioting, as would anyone who attempted to go in or help people inside. The police added that anyone inside the campus could peacefully leave via the exit at Y-core. However, those attempting to leave via the designated exit were instead arrested. Among the people arrested were university staff, reporters, social workers, volunteer first-aid personnel, doctors and nurses. Surgeon Darren Mann, who had witnessed medics being hogtied and who had telephoned both the Red Cross and Médecins Sans Frontières asking them to intervene, criticised the police for treating medical personnel like terrorists."
+                    },
+                    {
+                        imagePath: "assets/img/1118poly.gif",
+                        title: "17/18 November – PolyU - Escape",
+                        comment: ""
+                    },
+                    {
+                        imagePath: "assets/img/1118ymt.gif",
+                        title: "18 November - Kowloon Blossom",
+                        comment: ""
                     }
                 ]
 };
